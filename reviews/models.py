80e40cb74274cfe5e78a2e89f8c9e57c2a24ee93
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-06-22 16:20:09 +0900

    feat: common 앱 추가, reviews 모델 생성.

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from common.models import CommonModel
+
 
 # Create your models here.
+class Reviews(CommonModel):
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+    content = models.TextField()
