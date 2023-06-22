6172de10f677c1d64000f76866e2ba6b634ab3a6
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-06-22 14:16:37 +0900

    fix: 앱 이름 복수형으로 바꿈. - 암묵적인 규칙입니다.

--- core/settings.py
+++ core/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # django app
-    "cafe",
-    "review",
-    "user",
+    "cafes",
+    "reviews",
+    "users",
 ]
 
 MIDDLEWARE = [
