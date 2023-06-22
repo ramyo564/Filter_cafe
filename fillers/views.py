da0045086507f42a039e50edda69f75742e3de3f
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-06-22 19:36:02 +0900

    feat: 카페 CURD 주석 작성

--- fillers/views.py
+++ fillers/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    """
+    쿼리 매개 변수로 필터링 조건과 페이지 정보가 들어옴.
+
+    필터링 후 페이지에 맞는 최대 20개씩 카페 목록을 출력
+    """
+    pass
+
+
+def create(request):
+    """
+    로그인 했을 때만 가능
+
+    POST요청만 가능
+
+    데이터 파싱(post를 통해 온 데이터 사용할 수 있게 변환) 후
+    name, filler, address 정보 입력
+
+    owner는 토의 후 재수정 필요
+
+    카페 생성
+
+    index페이지로 리다일렉트
+
+    """
+    pass
+
+
+def put(request, cafe_pk):
+    """
+    로그인 유저인지 검사
+
+    put 요청인지 확인(put은 수정할 때 주로 보내는 요청. POST랑 비슷하지만 구분해서 사용함.)
+
+    cafe_pk가 있는지 검사
+
+    데이터 파싱 후 내용 수정.
+
+    리다이렉트 인덱스 페이지
+    """
+    pass
+
+
+def delete(request, cafe_pk):
+    """
+    로그인 유저인지 검사
+
+    delete요청인지 확인
+
+    cafe_pk가 있는지 검사
+
+    해당 PK에 해당하는 데이터 삭제
+
+    (이때 데이터를 복구가능하게 할지 아니면 그냥 영구 삭제할지)
+    """
+    pass
