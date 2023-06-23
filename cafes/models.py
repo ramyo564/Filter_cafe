2852795b84a6509d344126d0b99742d72308b94b
ramyo564 <yohan032yohan@gmail.com>
2023-06-23 09:30:35 +0900

    docs : 리뷰 모델 재구성
    - 리뷰 생성 날짜 재구성

--- cafes/models.py
+++ cafes/models.py
@@ -23,6 +23,8 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.IntegerField()
     reviews = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.reviews
