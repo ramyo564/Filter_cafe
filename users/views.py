9f2b143d22aa8e998f943232ba00952f4c2b268d
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-06-25 19:45:18 +0900

    feat: users views content

--- users/views.py
+++ users/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# 3 join community
+def signup(request):
+    """
+    회원가입
+    """
+
+
+def update_user(request):
+    """
+    유저 정보 수정
+    """
+
+
+# 회원가입 후에는 ID가 보임.
+def profile(request):
+    """
+    프로필 페이지 구성
+
+    """
+
+
+def login(request):
+    """
+    로그인
+    """
+
+
+def logout(request):
+    """
+    로그 아웃
+    """
+
+
+def delete(request):
+    """
+    회원 탈퇴
+    """
