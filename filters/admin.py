654925105be60bb81dc22d16e5b97ea03c7a1c61
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-06-27 17:08:30 +0900

    feat: 모델 makemigrations추가 및 admin 클래스 추가

--- filters/admin.py
+++ filters/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Filter
+from .models import BallotBox, Filter, FilterScore
 
 # Register your models here.
 
 
 admin.site.register(Filter)
+admin.site.register(FilterScore)
+admin.site.register(BallotBox)
