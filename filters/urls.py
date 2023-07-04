7127f26407bc04516b3908b8a56a47f1be592ff5
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-04 16:06:24 +0900

    feat: FilterScore 생성, 디테일, 테스트코드

--- filters/urls.py
+++ filters/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.CafeFilter.as_view()),
+    path("score/", views.CafeFilterScore.as_view()),
     path("<int:filter_pk>", views.CafeFilterDetail.as_view()),
+    path("score/<int:filter_score_pk>", views.CafeFilterScoreDetail.as_view()),
 ]
